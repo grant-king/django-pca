735e5deefd858db1ab482cfbbed4d2adc3ddcfc1
Grant King <grantryan.king@gmail.com>
2020-08-27 23:00:08 -0700

    Update models.py
    Add first model SessionConfiguration to store session configuration settings.

--- mysite/caconsole/models.py
+++ mysite/caconsole/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class SessionConfiguration(models.Model):
+    screen_size = models.CharField(max_length=10)
+    cell_size = models.IntegerField()
+    ruleset_name = models.CharField(max_length=50)
+    aging = models.BooleanField()
+    processing_mode = models.IntegerField(choices=[(1, 'CPU'), (2, 'GPU')])
+    show_colors = models.BooleanField()
+    seed_image_path = models.FilePathField(path='D:\chaos\extra')
