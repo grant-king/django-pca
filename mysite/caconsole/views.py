b5a75d88f3b482f621127acb857392f42bfbd46d
Grant King <grantryan.king@gmail.com>
2020-08-28 20:53:59 -0700

    Add view
    Add a new view to list all configurations.

--- mysite/caconsole/views.py
+++ mysite/caconsole/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import SessionConfiguration
 import pythoncellularautomata.models.session_models as pca
 
 
 def index(request):
+    return HttpResponse("You're at the cellular automata console index.")
+
+def play_default(request):
     session_manager = pca.SessionConfigurationManager()
     session_manager.play_default()
-    return HttpResponse("You're at the cellular automata console index.")
+    return HttpResponse("This page plays the default automaton.")
+
+def all_configurations(request):
+    all_configurations = SessionConfiguration.objects.all()
+    html_configs = '<br>'.join([config.name for config in all_configurations])
+    return HttpResponse(f'All configs:<br>{html_configs}')
+
