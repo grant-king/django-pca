e62841023c09b20ef629ce83c337ef99f2db4f83
Grant King <grantryan.king@gmail.com>
2020-08-28 20:47:47 -0700

    Add ca console app to intalled apps
    Register ca console app.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'caconsole.apps.CaconsoleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
