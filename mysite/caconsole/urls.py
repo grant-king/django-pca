33bb6a6e4178ac5485e8cff20a228fbad23f0963
Grant King <grantryan.king@gmail.com>
2020-09-09 21:56:57 -0700

    Update urls.py
    Rename path for playing default sim configuration.

--- mysite/caconsole/urls.py
+++ mysite/caconsole/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('configs/', views.all_configurations, name='all-configs'),
     path('configs/<int:configuration_id>/', views.configuration_detail, name='config-detail'),
-    path('play/default/', views.play_default, name='default-config'),
+    path('play/default/', views.play_default, name='play-default'),
     path('play/<int:configuration_id>/', views.play_configuration, name='run-config')
     ]
